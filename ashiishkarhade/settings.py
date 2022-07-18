69b883f2e86cf09fa84205003848ae6919180924
Ashish Karhade <akarhade5@gmail.com>
2022-07-18 16:17:25 +0530

    css serving issue try resolving

--- ashiishkarhade/settings.py
+++ ashiishkarhade/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,9 +25,10 @@ SECRET_KEY = 'django-insecure-gfoo_r!s)oj%+42&3!w)3n0@s2=5yq)bjr%v_98e(9wnixsaf9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ashiishkarhade-2022.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Application definition
 
