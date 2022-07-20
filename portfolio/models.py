d22197abee76bf5ebb3e584cc9a374975324563d
Ashish Karhade <akarhade5@gmail.com>
2022-07-20 21:27:27 +0530

    some changes done

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django import forms
+from datetime import date, datetime
+
 
 # Create your models here.
+
+class ContactForm(forms.Form):
+    name = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+
+class Work:
+    dates = date.today().strftime("%b %d, %y")
+    title = "Default Title"
+    description = "Default Description"
+    link = "https://www.google.com"
+    image_name = "photo-1"
+
+    def __init__(self, dates, title, description, link, image_name):
+        self.dates = datetime.strptime(dates, "%d%m%Y").date().strftime("%b %m, %Y")
+        self.title = title
+        self.description = description
+        self.link = link
+        self.image_name = image_name
+
+    def __str__(self):
+        print(self.title)
+
+
+class ProjectsClass(Work):
+    def __str__(self):
+        print("Project : " + self.title)
+
+
+class TalksClass(Work):
+    def __str__(self):
+        print("Talks : " + self.title)
+
+
+class BlogsClass(Work):
+    def __str__(self):
+        print("Blogs : " + self.title)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ContactForm, ProjectsClass, TalksClass, BlogsClass
+
+projects = [
+    ProjectsClass("14042021", "Whatsapp Data Analysis",
+                  "Analysing my personal group chat and finding out interesting insights about it",
+                  "https://deepnote.com/@ashish-karhade/whatsapp-data-analysis-ee8e7939-c4c7-44d7-967f-972aa6fac8de",
+                  "1"),
+    ProjectsClass("20122020", "Dog Breed Classifier",
+                  "Check the breed of the dog from image, or check which dog looks like you.",
+                  "https://github.com/AshiishKarhade/Dog-Breed-Classifier", "project-image-2"),
+    ProjectsClass("28042021", "COVID-19 Detection",
+                  "Detection using chest x-ray images from collecting 150 Images of Positive and Negative Images of Chest X-ray from COVID-19 Test.",
+                  "https://github.com/dsc-sits/COVID-19-X-RAY-Images-Detection", "project-image-3"),
+]
+
+talks = [
+    TalksClass("07012020", "Machine Learning Session @ KBTCOE, Nashik",
+               "Invited as guest speaker to conduct 2-Day Machine Learning Workshop", "#", "talk-image-1"),
+    TalksClass("17122019", "Explore ML Workshop @ SIT, Lonavala",
+               "2-Days Machine Learning workshop at Sinhgad Institutes.", "#", "talk-image-2"),
+    TalksClass("25072019", "ML Facilitator Program, Google",
+               "Selected as ML Facilitator and invited to Google Office, Hyderabad to learn ML from Googlers", "#", "talk-image-3"),
+]
+
+
+blogs = [
+   "blogs"
+]
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {"projects": projects,
+               "talks": talks,
+               "blogs": blogs
+               }
+    return render(request, 'index.html', context=context)
+
+
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail("Ashish Karhade - Contact", message, from_email, ['akarhade5@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "index.html", {'form': form})
+
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
